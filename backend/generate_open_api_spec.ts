ce000eabc78b462f4d126d28768670070fc8d6d2
Maximilian Hammerl <maximilian.hammerl@yorizon.com>
2026-08-17 19:42:28 +0200

    "openapi": "3.2.0",

--- backend/generate_open_api_spec.ts
+++ backend/generate_open_api_spec.ts
@@ -5,11 +5,13 @@ const OPEN_API_SPEC_PATH = "./open-api.json";
 
 if (import.meta.main) {
   console.log("Generating OpenAPI specification");
+
   const spec = app.getOpenAPI31Document(openApiSpecification);
-
   console.log("Generated successfully, saving OpenAPI specification to file");
-  await Deno.writeTextFile(OPEN_API_SPEC_PATH, JSON.stringify(spec));
 
+  spec.openapi = "3.2.0";
+
+  await Deno.writeTextFile(OPEN_API_SPEC_PATH, JSON.stringify(spec));
   console.log("Saved successfully");
 
   Deno.exit();
